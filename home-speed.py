75d87e238966c2bd35163e6b078a0ff959a07d6d
Fred Phillips <fphillips7@bloomberg.net>
2020-06-29 12:20:18 +0100

    remove schedule

--- home-speed.py
+++ home-speed.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+
 import dateutil.tz
-import schedule
-import time
+
 from influxdb import InfluxDBClient
 from speedtest import Speedtest
+
 
 def main():
     print("Conducting speed test")
@@ -13,33 +14,25 @@ def main():
 
     print(f"upload={upload} download={download} now={now}")
 
-    rows = [{
-        "measurement": "network_speed",
-        "tags": {
-            "direction": "upload"
+    rows = [
+        {
+            "measurement": "network_speed",
+            "tags": {"direction": "upload"},
+            "time": now.isoformat(),
+            "fields": {"value": upload},
         },
-        "time": now.isoformat(),
-        "fields": {
-            "value": upload
-        }
-    }, {
-        "measurement": "network_speed",
-        "tags": {
-            "direction": "download"
+        {
+            "measurement": "network_speed",
+            "tags": {"direction": "download"},
+            "time": now.isoformat(),
+            "fields": {"value": download},
         },
-        "time": now.isoformat(),
-        "fields": {
-            "value": download
-        }
-    }]
+    ]
 
-    client = InfluxDBClient('pi', 8086, 'influxdb', 'influxdb', 'db0')
+    client = InfluxDBClient("pi", 8086, "influxdb", "influxdb", "db0")
     client.write_points(rows)
     print("Complete!")
 
 
 if __name__ == "__main__":
-    schedule.every(2).minutes.do(main)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    main()
